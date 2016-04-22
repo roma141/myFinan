375925395b107c7a67b0610a22b4d73562d79df5
Andres Botello <roma141@gmail.com>
2016-04-22 17:41:12 -0500

    better for mobile

--- js/jgoogle.js
+++ js/jgoogle.js
@@ -4,15 +4,15 @@
 // 	if (typeof servidor == 'undefined' || servidor=="" || servidor==null)
 // 		servidor = "192.168.1.105:8084";
 // }
-//   function leeServidor()
-// {
-//  	ayuda = "http://wiki.gtienda.com";
-//  	if (typeof servidor == 'undefined' || servidor=="" || servidor==null)
-//  		servidor = "127.0.0.1:8084";
-// }
-function leeServidor()
+  function leeServidor()
 {
-	ayuda = "http://wiki.gtienda.com";
-	if (typeof servidor == 'undefined' || servidor=="" || servidor==null)
-		servidor = "botpi.com:8084";
+ 	ayuda = "http://wiki.gtienda.com";
+ 	if (typeof servidor == 'undefined' || servidor=="" || servidor==null)
+ 		servidor = "127.0.0.1:8084";
 }
+// function leeServidor()
+// {
+// 	ayuda = "http://wiki.gtienda.com";
+// 	if (typeof servidor == 'undefined' || servidor=="" || servidor==null)
+// 		servidor = "botpi.com:8084";
+// }
