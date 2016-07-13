4434d456a14cb533de7197e85a0b4e470753ba4a
Andres Botello <roma141@gmail.com>
2016-07-13 16:50:18 -0500

    work on localhost

--- js/jgoogle.js
+++ js/jgoogle.js
@@ -4,5 +4,6 @@ function leeServidor()
  	ayuda = "http://wiki.gtienda.com";
  	if (typeof servidor == 'undefined' || servidor=="" || servidor==null)
  		// servidor = "192.168.1.100:8084";
-		servidor = "botpi.com:8084";
+ 		servidor = "localhost:8084";
+		// servidor = "botpi.com:8084";
 }
