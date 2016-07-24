22eb6c30d53dec52f9df2c67dcc92549022224c9
Andres Botello <roma141@gmail.com>
2016-07-24 17:52:50 -0500

    analitycs

--- js/jgoogle.js
+++ js/jgoogle.js
@@ -8,3 +8,10 @@ function leeServidor()
  		servidor = "192.168.2.109:8084";
 		// servidor = "botpi.com:8084";
 }
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+// })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+// ga('create', 'UA-81270544-1', 'auto');
+// ga('send', 'pageview');
