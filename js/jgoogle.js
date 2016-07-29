69e3c21593ec469108f2fd0051837d81db45f0a9
Andres Botello <roma141@gmail.com>
2016-07-29 18:06:42 -0500

    try

--- js/jgoogle.js
+++ js/jgoogle.js
@@ -7,9 +7,6 @@ function leeServidor()
  		// servidor = "localhost:8084";
  		// servidor = "192.168.2.109:8084";
 		servidor = "botpi.com:8084";
-	window.ga.startTrackerWithId('UA-81270544-2');
-	window.ga.trackView(String(document.title));
-	window.ga.setAppVersion('1.0.1');
 }
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -18,3 +15,6 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-81270544-1', 'auto');
 ga('send', 'pageview');
+window.ga.startTrackerWithId('UA-81270544-2');
+window.ga.trackView(String(document.title));
+window.ga.setAppVersion('1.0.1');
