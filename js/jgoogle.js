9580bfebcecf4b3dda94f87e16e483156973ba73
Andres Botello <roma141@gmail.com>
2016-07-30 16:55:23 -0500

    try

--- js/jgoogle.js
+++ js/jgoogle.js
@@ -1,6 +1,19 @@
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+function onDeviceReady() {
+    // Now safe to use device APIs
+    // window.ga.startTrackerWithId('UA-81270544-2');
+    window.ga.startTrackerWithId('UA-81270544-2');
+	// window.ga.trackView('Screen Title');
+	// window.ga.trackView(String(document.title));
+	window.ga.trackView(document.title);
+	// window.ga.setAppVersion('1.0.1');
+}
 function leeServidor()
 {
  	ayuda = "http://wiki.gtienda.com";
+ 	onLoad();
  	if (typeof servidor == 'undefined' || servidor=="" || servidor==null)
  		// servidor = "192.168.1.100:8084";
  		// servidor = "localhost:8084";
@@ -14,8 +27,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-81270544-1', 'auto');
 ga('send', 'pageview');
-
-var analytics = navigator.analytics;
- 
-analytics.setTrackingId('UA-81270544-2');
-analytics.sendAppView(document.title, successCallback, errorCallback);
